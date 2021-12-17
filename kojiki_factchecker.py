4bfbe55cec09397c2dfd77c5aff53e3e0328f32d
YamatoKuroneko <mofutochang@gmail.com>
2021-12-18 01:31:02 +0900

    空白の文字列が入力されたときの例外処理を追加。

--- kojiki_factchecker.py
+++ kojiki_factchecker.py
@@ -11,14 +11,17 @@ def lf_cleaner(s):
 
 with open("kojiki.txt", encoding="UTF-8") as k:
     input_str = lf_cleaner(input("検証する文字列を入力:"))
-    kojiki = k.read()
-    for i in range(len(kojiki)):
-        if input_str[0] == kojiki[i]:
-            for j in range(len(input_str)):
-                if input_str[j] != kojiki[i+j]:
-                    break
-            else:
-                print("古事記にもそう書いてある。")
-                break
+    if input_str == "":
+        print("空白の文字列です。")
     else:
-        print("古事記にはそう書いていない。")
+        kojiki = k.read()
+        for i in range(len(kojiki)):
+            if input_str[0] == kojiki[i]:
+                for j in range(len(input_str)):
+                    if input_str[j] != kojiki[i+j]:
+                        break
+                else:
+                    print("古事記にもそう書いてある。")
+                    break
+        else:
+            print("古事記にはそう書いていない。")
